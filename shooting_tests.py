fcb6b7d765fc51c90a502bcc3fc77b17d56d3b00
em18029 <em18029@bristol.ac.uk>
2022-04-22 16:34:06 +0100

    basic numerical shooting tests

--- shooting_tests.py
+++ shooting_tests.py
@@ -1,13 +1,42 @@
-from shooting import numerical_shooting
-from ode import hopf_birfurcation
-import math
-
-def test(result,known_result):
-    if math.close(result,known_result,abs_tol = 1e-4):
-        print('Test Passed')
-    else:
-        print('Test Failed')
-    return
+import unittest
+import numpy as np
+from shooting import numericalShooting
+from odes import odes
 
 
-#result =
+class MyTestCase(unittest.TestCase):
+    # def test_nullcline(self):
+    #     [u1,u2] = nullcline(odes.pred_prey,[0,1])
+    #     assert np.allclose([])
+
+    def test_gen_ode_data(self):
+        self.assertIsNotNone(nS.ode_data, 'ODE data generated')
+
+    def test_gen_t_data(self):
+        self.assertIsNotNone(nS.t_data, 'Time data generated')
+
+    def test_data_shape(self):
+        data_shape = np.shape(nS.ode_data)
+        self.assertEqual((u0[-1] / nS.max_step + 1, len(args[:-1])), data_shape, 'Data generated is the correct shape')
+
+    def test_extrema(self):
+        self.assertIsNotNone(nS.extrema, 'Extrema of odes found')
+
+    def test_periods_close(self):
+        self.assertAlmostEqual(nS.find_period(nS.ode_data[:, 0]), nS.find_period(nS.ode_data[:, 0]))
+
+    def test_shooting_conditions(self):
+        self.assertIsNotNone(final,'Shooting conditions outputted')
+
+    #def test
+
+if __name__ == '__main__':
+    u0 = np.array([1, 0.5, 100]);
+    args = np.array([1, 0.2, 0.1]);
+    myode = odes.pred_prey
+    nS = numericalShooting(myode, u0, args)
+    nS.gen_data(args)
+    nS.isolate_orbit()
+    final = nS.shooting()
+
+    unittest.main()
