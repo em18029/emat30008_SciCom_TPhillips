fc8857f9c7d70d1bc28be92bdbd230034dcdd1b7
em18029 <em18029@bristol.ac.uk>
2022-04-26 23:24:43 +0100

    edits in isolate orbit

--- shooting.py
+++ shooting.py
@@ -2,9 +2,16 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 from scipy.signal import argrelextrema
+from scipy.signal import find_peaks
 from scipy.integrate import solve_ivp
 from scipy.optimize import fsolve
-from odes import pred_prey
+from odes import odes
+
+'''
+Add plotting one limit cycle and other full data from odes
+Add checking inputs
+try get isclsoe to work
+'''
 
 
 class numericalShooting:
@@ -14,6 +21,10 @@ class numericalShooting:
         self.t_data = None
         self.u0 = u0
         self.args = args
+        self.max_step = 1e-2
+        self.x_data = None
+        self.y_data = None
+        self.final = None
 
     def gen_data(self, args):
         '''
@@ -31,9 +42,10 @@ class numericalShooting:
         x0 = self.u0[:-1]
         T = self.u0[-1]
         t_data = (0, T)
-        ode_data = solve_ivp(self.ode, t_data, x0, max_step=1e-2, args=args)
+        ode_data = solve_ivp(self.ode, t_data, x0, max_step=self.max_step, args=args)
         self.ode_data = np.transpose(ode_data.y)
         self.t_data = np.transpose(ode_data.t)
+        return self.ode_data,self.t_data
 
     def isolate_orbit(self):
         '''
@@ -43,30 +55,24 @@ class numericalShooting:
         -------
         An update to u0 where x_data and y_data are boundary values found and and an update on period.
         '''
-        x_data = self.ode_data[:, 0]
-        y_data = self.ode_data[:, 1]
-        # plt.plot(t_data,x_data)
-        # plt.show()
-        extrema = argrelextrema(x_data, np.greater)[0]
+        self.x_data, self.y_data = self.ode_data[:, 0], self.ode_data[:, -1]
         prev_val = False
         prev_t = 0
-
+        #extrema = argrelextrema(self.x_data, np.greater)[0]
+        extrema = find_peaks(self.x_data)[0]
         for i in extrema:
             if prev_val:
-                # if math.isclose(x_data[i], prev_val, abs_tol=1e-4):
-                period = self.t_data[i] - prev_t
-                self.u0 = [x_data[i], y_data[i], period]
-                return
-            prev_val = x_data[i]
+                if math.isclose(self.x_data[i], prev_val, rel_tol=1):
+                    period = self.t_data[i] - prev_t
+                    self.u0 = [self.x_data[i], self.y_data[i], period]
+                return self.u0
+            prev_val = self.x_data[i]
             prev_t = self.t_data[i]
-        return
+        raise RuntimeError("No orbit found")
 
     def shooting_conditions(self, u0, ode, args):
         '''
         A function which utilises scipy's solve_ivp to find the starting
-
-
-
         '''
         x0 = u0[:-1]
         t0 = u0[-1]
@@ -93,27 +99,61 @@ class numericalShooting:
         returned array is empty.
         """
         final = fsolve(self.shooting_conditions, self.u0, args=(self.ode, self.args))
+        self.final = final
         return final
 
-    def main( ode, u0, args):
+    def main_loop(ode, u0, args):
         nS = numericalShooting(ode, u0, args)
         nS.gen_data(args)
         nS.isolate_orbit()
-        print('Initial starting conditions', nS.u0)
-        print('Initial limit conditions', nS.shooting())
+        # print('Initial starting conditions', nS.u0)
+        # print('Initial limit conditions', nS.shooting())
+        final = nS.shooting()
+        # nS.plot_sols(args)
+        return final
 
-        return nS.shooting()
+    def plot_sols(self, args):
+        plt.subplot(1, 2, 1)
+        plt.plot(self.t_data, self.x_data, 'r-', label='a')
+        plt.plot(self.t_data, self.y_data, 'b-', label='b')
+        plt.grid()
+        plt.legend(loc='best')
+        plt.xlabel('t')
+        plt.ylabel('x')
+        plt.subplot(1, 2, 2)
+        self.u0 = self.final
+        print('u0', self.u0)
+        nS.gen_data(args)
+        plt.plot(self.t_data, self.ode_data[:, 0], 'r-', label='a')
+        plt.plot(self.t_data, self.ode_data[:, 1], 'b-', label='b')
+        plt.plot([0, self.final[-1]], [self.final[0], self.final[0]], 'ro')
+        plt.plot([0, self.final[-1]], [self.final[1], self.final[1]], 'bo')
+        plt.grid()
+        plt.legend(loc='best')
+        plt.xlabel('t')
+        plt.ylabel('x')
+        plt.show()
 
 
 if __name__ == '__main__':
+    # '''
+    # Predator Prey equations init conditions
+    # '''
+    # args = np.array([1, 0.25, 0.1])
+    # u0 = [5, 10, 200]
+    # myode = odes.pred_prey
+    #
     '''
-    Predator Prey equations init conditions
+    Hof-Bifurcation equtions
     '''
-    args = np.array([1, 0.2, 0.1])
-    u0 = [1, 0.5, 100]
-    nS = numericalShooting(pred_prey, u0, args)
+    args = np.array([0.1, -1])
+    u0 = [1.5, 0, 20]
+    myode = odes.hopf_bifurcation
 
+    nS = numericalShooting(myode, u0, args)
     nS.gen_data(args)
     nS.isolate_orbit()
     print('Initial starting conditions', nS.u0)
     print('Initial limit conditions', nS.shooting())
+
+    nS.plot_sols(args)
